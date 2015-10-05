21343ffb878d2ab9940a745bd238d8b3262736cb
Josh Jeffryes <jjeffryes@gmail.com>
2015-10-05 15:21:31 -0400

    Minor change to loadTemplate

--- js/utils/loadTemplate.js
+++ js/utils/loadTemplate.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 
 module.exports = function(templateFile, callback){
 
-  fs.readFile(templateFile, "utf8", function(err, data){
+  fs.readFile(templateFile, "utf8", function(err, ob){
     if (err) {
       throw err;
     }
     __.templateSettings.variable = "ob";
-    var tmpl = __.template(data);
+    var tmpl = __.template(ob);
     callback(tmpl);
   });
 };
