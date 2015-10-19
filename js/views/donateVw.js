8c31828c039dde617cb3b549c464b5f33f6b104c
Josh Jeffryes <jjeffryes@gmail.com>
2015-10-19 14:43:26 -0400

    Fixes to about and donate

--- js/views/donateVw.js
+++ js/views/donateVw.js
@@ -17,7 +17,7 @@ module.exports = Backbone.View.extend({
 
   render: function(){
     var self = this;
-    
+    $('#content').html(this.$el);
     loadTemplate('./js/templates/donate.html', function(loadedTemplate) {
       self.$el.html(loadedTemplate(self.model.toJSON()));
     });
