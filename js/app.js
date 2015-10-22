22bb5ae6bed0324c6c5fc6f96f293e29ae6592a4
Josh Jeffryes <jjeffryes@gmail.com>
2015-10-22 16:16:23 -0400

    Reload on start, error handling, item error handling

--- js/app.js
+++ js/app.js
@@ -21,9 +21,13 @@ var Polyglot = require('node-polyglot'),
     languages = new languagesModel(),
     socketView = require('./views/socketVw');
     guid = "",
-    cCode = '',
-    server_urlLocal = localStorage.getItem("server_url") || "http://localhost:18469/api/v1/";
+    cCode = "",
+    server_urlLocal = "",
+    loadProfileCount = 0;
 
+var loadProfileTimeout;
+
+server_urlLocal = localStorage.getItem("server_url") || "http://localhost:18469/api/v1/",
 
 //set the urlRoot of the user model. Defaults to local host if not found
 user.urlRoot = server_urlLocal + "settings";
@@ -43,51 +47,92 @@ window.polyglot.extend(__.where(languages.get('languages'), {langCode: window.la
 //put the event bus into the window so it's available everywhere
 window.obEventBus =  __.extend({}, Backbone.Events);
 
-//get the guid from the user profile to put in the user model
+var loadProfile = function() {
+  //get the guid from the user profile to put in the user model
+  userProfile.fetch({
+    success: function (model, response) {
+      "use strict";
+      //make sure profile is not blank
+      if (response.profile){
+        guid = model.get('profile').guid;
+        console.log(guid);
+        //get the user
+        user.fetch({
+          success: function (model, response) {
+            console.log(response);
+            if (response.ssl){
+              user.set('server_url', server_urlLocal);
+              user.set('guid', guid);
+              cCode = model.get('currency_code');
 
-userProfile.fetch({
-  success: function(model){
-    "use strict";
-    guid = model.get('profile').guid;
-    //get the user
-    user.fetch({
-      success: function(model){
-        user.set('server_url', server_urlLocal);
-        user.set('guid', guid);
-        cCode = model.get('currency_code');
+              //get user bitcoin price before loading pages
+              getBTPrice(cCode, function (btAve) {
+                //put the current bitcoin price in the window so it doesn't have to be passed to models
+                window.currentBitcoin = btAve;
+                //every 15 minutes update the bitcoin price
+                setTimeout(function () {
+                  getBTPrice(cCode, function (btAve) {
+                    window.currentBitcoin = btAve;
+                  });
+                }, 54000000);
 
-        //get user bitcoin price before loading pages
-        getBTPrice(cCode, function(btAve){
-          //put the current bitcoin price in the window so it doesn't have to be passed to models
-          window.currentBitcoin = btAve;
-          //every 15 minutes update the bitcoin price
-          setTimeout(function(){
-            getBTPrice(cCode, function(btAve){window.currentBitcoin = btAve;});
-          },54000000);
-
-          $('.js-loadingModal').hide();
-          new pageNavView({model: user});
-          new router({userModel: user, socketView: new socketView({model: user})});
-          Backbone.history.start();
+                $('.js-loadingModal').hide();
+                new pageNavView({model: user});
+                new router({userModel: user, socketView: new socketView({model: user})});
+                Backbone.history.start();
+              });
+            }else{
+              $('.js-indexLoadingMsg1').text("User model did not load.");
+              $('.js-indexLoadingMsg2').text("Attempting to reach " + server_urlLocal);
+              $('.js-indexLoadingMsg3').text("Reload attempt " + loadProfileCount);
+              reloadProfile();
+              console.log("failed user ssl");
+            }
+          },
+          error: function (model, response) {
+            console.log("Information for user could not be loaded: " + response.statusText);
+            alert("No user was found. Your server may not be working correctly. Loading using default settings.");
+            $('.js-loadingModal').hide();
+            user.set('server_url', server_urlLocal);
+            new pageNavView({model: user});
+            new router({userModel: user});
+            Backbone.history.start();
+          }
         });
-      },
-      error: function(model, response){
-        console.log("Information for user could not be loaded: " + response.statusText);
-        alert("No user was found. Your server may not be working correctly. Loading using default settings.");
-        $('.js-loadingModal').hide();
-        user.set('server_url', server_urlLocal);
-        new pageNavView({model: user});
-        new router({userModel: user});
-        Backbone.history.start();
+      }else{
+        $('.js-indexLoadingMsg1').text("User profile did not load.");
+        $('.js-indexLoadingMsg2').text("Attempting to reach " + server_urlLocal);
+        $('.js-indexLoadingMsg3').text("Reload attempt " + loadProfileCount);
+        reloadProfile();
+        console.log("failed user profile");
       }
-    });
-  },
-  error: function(model, response){
-    console.log("Information for the user profile could not be loaded: " + response.statusText);
-    alert("No user was found. Your server may not be working correctly. Loading using default settings.");
-  }
-});
+    },
+    error: function (model, response) {
+      $('.js-indexLoadingMsg1').text("Information for your user profile could not be loaded: " + response.statusText);
+      $('.js-indexLoadingMsg2').text("Attempting to reach " + server_urlLocal);
+      $('.js-indexLoadingMsg3').text("Reload attempt " + loadProfileCount);
+      reloadProfile();
+      console.log("error on main fetch");
+    }
+  });
+};
 
+var reloadProfile = function(){
+  "use strict";
+  if(loadProfileCount < 10){
+    loadProfileTimeout = window.setTimeout(function(){
+      loadProfileCount++;
+      loadProfile();
+    }, 3000);
+  } else {
+    $('.js-indexLoadingMsg1').text(server_urlLocal + "cannot be reached.");
+    $('.js-indexLoadingMsg2').text("Check your server and restart the client application");
+    $('.js-indexLoadingMsg3').text("");
+    window.clearTimeout(loadProfileTimeout);
+  }
+};
+
+loadProfile();
 
 
 
