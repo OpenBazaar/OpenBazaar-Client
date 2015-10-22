1cf2e2aa1da12a365bcbe2f2b0ceb76d5b37234a
Brian Hoffman <brianchoffman@gmail.com>
2015-10-22 13:57:06 -0400

    Convert to path.sep

--- js/utils/loadTemplate.js
+++ js/utils/loadTemplate.js
@@ -1,12 +1,13 @@
 var __ = require('underscore');
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function(templateFile, callback){
 
   var loc = window.location.pathname;
-  var cur_dir = loc.substring(0, loc.lastIndexOf('/'));
+  var cur_dir = loc.substring(0, loc.lastIndexOf(path.sep));
 
-  fs.readFile(cur_dir + '/' + templateFile, "utf8", function(err, ob){
+  fs.readFile(cur_dir + path.sep + templateFile, "utf8", function(err, ob){
 
     if (err) {
       throw err;
