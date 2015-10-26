9e566cb0eb7ef522de80986301158964afd56ede
Josh Jeffryes <jjeffryes@gmail.com>
2015-10-26 14:54:03 -0400

    Adds navigation by Guid

--- js/views/pageNavVw.js
+++ js/views/pageNavVw.js
@@ -8,7 +8,8 @@ var __ = require('underscore'),
     currencyListView = require('../views/currencyListVw'),
     languageListView = require('../views/languageListVw'),
     adminPanelView = require('../views/adminPanelVw'),
-    remote = require('remote');
+    remote = require('remote'),
+    currentWindow;
 
 module.exports = Backbone.View.extend({
 
@@ -21,6 +22,7 @@ module.exports = Backbone.View.extend({
     'click .js-navBack': 'navBackClick',
     'click .js-navFwd': 'navFwdClick',
     'click .js-navProfile': 'navProfileClick',
+    'click .js-navRefresh': 'navRefreshClick',
     'click .js-navAdminPanel': 'navAdminPanel',
     'click .js-navProfileMenu a': 'closeNav',
     'click .js-homeModal-countrySelect': 'countrySelect',
@@ -32,13 +34,18 @@ module.exports = Backbone.View.extend({
     'click .js-homeModal-cancelHandle': 'cancelHandle',
     'change .js-homeModalAvatarUpload': 'uploadAvatar',
     'click .js-homeModalDone': 'settingsDone',
-    'click .js-closeModal': 'closeModal'
+    'click .js-closeModal': 'closeModal',
+    'keyup .js-navAddressBar': 'addressBarKeyup',
+    'click .js-navAddressBarGo': 'addressBarGoClick',
+    'click .js-closeStatus': 'closeStatusBar'
   },
 
   initialize: function(){
+    "use strict";
     var self = this;
     this.subViews = [];
     this.languages = new languagesModel();
+    this.currentWindow = remote.getCurrentWindow();
 
   //when language is changed, re-render
     this.listenTo(this.model, 'change:language', function(){
@@ -54,6 +61,7 @@ module.exports = Backbone.View.extend({
   },
 
   initAccordion: function(targ){
+    "use strict";
     var acc = $(targ);
     var accWidth = acc.width();
     var accHeight = acc.height();
@@ -93,21 +101,23 @@ module.exports = Backbone.View.extend({
   },
 
   closeNav: function(){
+    "use strict";
     var targ = this.$el.find('.js-navProfileMenu');
     targ.addClass('hide');
     $('#overlay').addClass('fadeOut hide');
   },
 
   render: function(){
+    "use strict";
     var self = this;
     loadTemplate('./js/templates/pageNav.html', function(loadedTemplate) {
       self.$el.html(loadedTemplate(self.model.toJSON()));
       self.countryList = new countryListView({el: '.js-homeModal-countryList', selected: self.model.get('country')});
       self.currencyList = new currencyListView({el: '.js-homeModal-currencyList', selected: self.model.get('currency_code')});
       self.languageList = new languageListView({el: '.js-homeModal-languageList', selected: self.model.get('language')});
-      self.subViews.push(this.countryList);
-      self.subViews.push(this.currencyList);
-      self.subViews.push(this.languageList);
+      self.subViews.push(self.countryList);
+      self.subViews.push(self.currencyList);
+      self.subViews.push(self.languageList);
       self.initAccordion('.js-profileAccordion');
       if(self.model.get('beenSet')){
         self.$el.find('.js-homeModal').hide();
@@ -115,11 +125,15 @@ module.exports = Backbone.View.extend({
       //add the admin panel
       self.adminPanel = new adminPanelView({model: self.model});
       self.subViews.push(self.adminPanel);
+      self.addressInput = self.$el.find('.js-navAddressBar');
+      self.addressBarGoBtn = self.$el.find('.js-navAddressBarGo');
+      self.statusBar = self.$el.find('.js-navStatusBar');
     });
     return this;
   },
 
   navProfileClick: function(e){
+    "use strict";
     e.stopPropagation();
     var targ = this.$el.find('.js-navProfileMenu');
     if(targ.hasClass('hide')){
@@ -139,34 +153,90 @@ module.exports = Backbone.View.extend({
   },
 
   navCloseClick: function(){
-    var win = remote.getCurrentWindow();
+    "use strict";
     var process = remote.process;
     if (process.platform != 'darwin') {
-      win.close();
+      this.currentWindow.close();
     } else {
-      win.hide();
+      this.currentWindow.hide();
     }
   },
 
   navMinClick: function(){
-    var win = remote.getCurrentWindow();
-    win.minimize();
+    "use strict";
+    this.currentWindow.minimize();
   },
 
   navMaxClick: function(){
-    var win = remote.getCurrentWindow();
-    win.maximize();
+    "use strict";
+    this.currentWindow.maximize();
   },
 
   navBackClick: function(){
+    "use strict";
     window.history.back();
   },
 
   navFwdClick: function(){
+    "use strict";
     window.history.forward();
   },
 
+  navRefreshClick: function(){
+    "use strict";
+    this.currentWindow.reload();
+  },
+
+  addressBarKeyup: function(e){
+    "use strict";
+    var barText = this.addressInput.val();
+    if(barText.length > 0){
+      //detect enter key
+      if (e.keyCode == 13){
+        this.addressBarProcess(barText);
+        this.addressBarGoBtn.addClass("fadeOut");
+      } else {
+        this.addressBarGoBtn.removeClass("fadeOut");
+        this.closeStatusBar();
+      }
+    } else {
+      this.addressBarGoBtn.addClass("fadeOut");
+      this.closeStatusBar();
+    }
+  },
+
+  addressBarGoClick: function(){
+    "use strict";
+    this.addressBarProcess(this.addressInput.val());
+    this.addressBarGoBtn.addClass("fadeOut");
+  },
+
+  addressBarProcess: function(addressBarText){
+    "use strict";
+    console.log("process "+addressBarText);
+    if(addressBarText.charAt(0) == "@"){
+      this.showStatusBar('Navigation by handle is not supported yet.');
+    } else if(addressBarText.length === 40){
+      Backbone.history.navigate('#userPage/'+addressBarText, {trigger:true});
+    } else {
+      this.showStatusBar('This is not a valid user GUID or handle.');
+    }
+  },
+
+  showStatusBar: function(msgText){
+    "use strict";
+    console.log("show statusbar");
+    this.statusBar.find('.js-statusBarMessage').text(msgText);
+    this.statusBar.removeClass('fadeOut');
+  },
+
+  closeStatusBar: function(){
+    "use strict";
+    this.statusBar.addClass('fadeOut');
+  },
+
   countrySelect: function(e){
+    "use strict";
     var targ = $(e.currentTarget);
     var ctry = targ.attr('data-code');
     $('.js-homeModal-countryList').find('input[type=radio]').prop("checked", false);
@@ -175,6 +245,7 @@ module.exports = Backbone.View.extend({
   },
 
   currencySelect: function(e){
+    "use strict";
     var targ = $(e.currentTarget); //TODO: Rename variables to be more readable
     //var crcy = targ.attr('data-name');
     var ccode = targ.attr('data-code');
@@ -185,6 +256,7 @@ module.exports = Backbone.View.extend({
   },
 
   languageSelect: function(e){
+    "use strict";
     var targ = $(e.currentTarget); //TODO: Rename variables to be more readable
     var lang = targ.attr('data-code');
     $('.js-homeModal-languageList').find('input[type=radio]').prop("checked", false);
@@ -193,6 +265,7 @@ module.exports = Backbone.View.extend({
   },
 
   timeSelect: function(e){
+    "use strict";
     var inpt = $(e.target).closest('input[type=radio]'); //TODO: Rename variables to be more readable
     var tz = inpt.attr('id');
     $('.js-homeModal-timezoneList').find('input[type=radio]').prop("checked", false);
@@ -201,22 +274,26 @@ module.exports = Backbone.View.extend({
   },
 
   newHandle: function(e){
+    "use strict";
     this.$el.find('.js-homeModal-handleInput').closest('.flexRow').removeClass('hide');
     this.$el.find('.js-homeModal-existingHandle').parent().addClass('hide');
     this.$el.find('.js-homeModal-cancelHandle').parent().removeClass('hide');
   },
 
   existingHandle: function(e){
+    "use strict";
     //TODO: add code to connect handle here
   },
 
   cancelHandle: function(e){
+    "use strict";
     this.$el.find('.js-homeModal-handleInput').closest('.flexRow').addClass('hide');
     this.$el.find('.js-homeModal-existingHandle').parent().removeClass('hide');
     this.$el.find('.js-homeModal-cancelHandle').parent().addClass('hide');
   },
 
   uploadAvatar: function(e){
+    "use strict";
     var self = this;
     var reader = new FileReader();
     reader.onload = function (e) {
@@ -228,16 +305,19 @@ module.exports = Backbone.View.extend({
   },
 
   settingsDone: function(e){
+    "use strict";
     this.model.set('beenSet',true);
     this.$el.find('.js-homeModal').hide();
   },
 
   closeModal: function(e){
+    "use strict";
     console.log("close modal")
     $(e.target).closest('.modal').addClass('fadeOut');
   },
 
   navAdminPanel: function(){
+    "use strict";
     this.$el.find('.js-adminModal').removeClass('fadeOut');
     this.adminPanel.updatePage();
   },
