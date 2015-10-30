398347d550b6b83bdbd2fe12a7ed23d46fdeebf9
Mike <mwolf84@gmail.com>
2015-10-30 12:34:13 -0500

    fixed small typo

--- js/models/languagesMd.js
+++ js/models/languagesMd.js
@@ -42,7 +42,7 @@ module.exports = Backbone.Model.extend({
         Website: "Website",
         Welcome: "Welcome to OpenBazaar",
         CreateStore: "Create Store",
-        SearchForItemsPlaceholder: "Search by items",
+        SearchForItemsPlaceholder: "Search for items",
         SearchForFollowersPlaceholder: "Search by name or handle",
         EstDeliveryDomesticPlaceholder: "Enter domestic shipping time e.g. 3-5 days",
         EstDeliveryInternationalPlaceholder: "Enter international shipping time e.g. 7-15 days",
