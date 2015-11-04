c3bd391a85fd3248ce0b1f7c7114ff773d1ed2f2
Josh Jeffryes <jjeffryes@gmail.com>
2015-11-04 10:16:57 -0500

    Fixed scroll to item, added some translations.

--- js/views/userPageVw.js
+++ js/views/userPageVw.js
@@ -317,10 +317,13 @@ module.exports = Backbone.View.extend({
 
     if(state === "item"){
       this.renderItem(hash);
+      $('#obContainer').scrollTop(367);
     }else if(state === "itemOld") {
       this.tabClick(this.$el.find(".js-storeTab"), this.$el.find(".js-item"));
+      $('#obContainer').scrollTop(367);
     }else if(state === "itemNew") {
       this.tabClick(this.$el.find(".js-storeTab"), this.$el.find(".js-store"));
+      $('#obContainer').scrollTop(367);
       this.sellItem();
     }else if(state){
       this.tabClick(this.$el.find(".js-" + state + "Tab"), this.$el.find(".js-" + state));
@@ -451,7 +454,6 @@ module.exports = Backbone.View.extend({
     });
     this.itemList = new itemListView({model: model, el: '.js-list3', userModel: this.options.userModel});
     this.subViews.push(this.itemList);
-    $('#obContainer').animate({ scrollTop: "367px" }, 0); // Josh not feeling great about the location of this. Basically I want to scroll past the header instantly when hitting a item detail view. With it here, it results in a strange loading flash.
   },
 
   renderFollowers: function (model) {
