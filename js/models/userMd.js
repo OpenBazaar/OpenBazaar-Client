476d4a7346f21f2cec45a0a9dcfc0982dcc74019
Josh Jeffryes <jjeffryes@gmail.com>
2015-11-11 21:06:41 -0500

    Don't use settings for serverUrl

--- js/models/userMd.js
+++ js/models/userMd.js
@@ -1,4 +1,5 @@
-var Backbone = require('backbone');
+var Backbone = require('backbone'),
+    countriesMd = require('./countriesMd');
 
 module.exports = Backbone.Model.extend({
   defaults: {
@@ -23,9 +24,9 @@ module.exports = Backbone.Model.extend({
     blocked: [], //a list of guids to block (LIST of 40 character hex strings)
     libbitcoin_server: "", //the server address (url string)
     ssl: true, //use ssl on the openbazaar server (“True” or “False”)
-    server_url: "http://localhost:18469/api/v1/", //set from localStorage
+    serverUrl: "http://localhost:18469/api/v1/", //set from localStorage
     //value below for testing. Real value should be dynamically set
-    //server_url: "http://seed.openbazaar.org:18469/api/v1/",
+    //serverUrl: "http://seed.openbazaar.org:18469/api/v1/",
     terms_conditions: "No terms or conditions", //default terms/conditions (string)
     refund_policy: "No refund policy" //default refund policy (string)
   }
