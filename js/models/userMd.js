cb5634693cc03cd2eddfe5a239626befb080f2c6
Josh Jeffryes <jjeffryes@gmail.com>
2015-11-15 17:36:42 -0500

    handle no value for currency_code

--- js/models/userMd.js
+++ js/models/userMd.js
@@ -37,7 +37,7 @@ module.exports = Backbone.Model.extend({
     "use strict";
 
     //make sure currency code is in all caps
-    response.currency_code = response.currency_code.toUpperCase();
+    response.currency_code = response.currency_code ? response.currency_code.toUpperCase() : "";
 
     //find the human readable name for the country
     var matchedCountry = this.countryArray.filter(function(value, i){
