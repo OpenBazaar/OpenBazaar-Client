2b53adf58fd753c852fc5bf7821850203db1a0e8
Josh Jeffryes <jjeffryes@gmail.com>
2015-12-10 19:49:25 -0500

    Switch modals to hide
    - fixes nav menu becoming unclickable due to opacity 0 modals over it.

--- js/utils/showErrorModal.js
+++ js/utils/showErrorModal.js
@@ -3,7 +3,7 @@ var $ = require('jquery');
 module.exports = function(errorTitle, errorMessage) {
   "use strict";
   var errorModal = $('.js-messageModal');
-  errorModal.removeClass('fadeOut');
+  errorModal.removeClass('hide');
   errorModal.find('.js-messageModal-title').html(errorTitle);
   errorModal.find('.js-messageModal-message').html(errorMessage);
 };
