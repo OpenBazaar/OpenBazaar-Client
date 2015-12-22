0fc6188394834dce1c41869bb3d5bc62789c2cf0
Brian Hoffman <brianchoffman@gmail.com>
2015-12-22 11:52:45 -0500

    add ipc modules

--- valiquire-redirect.js
+++ valiquire-redirect.js
@@ -1,5 +1,5 @@
 'use strict';
-var electronModules = ['electron', 'app', 'browser-window', 'crash-reporter', 'remote', 'shell', 'clipboard', 'screen', 'tray', 'menu', 'auto-updater'];
+var electronModules = ['electron', 'app', 'browser-window', 'crash-reporter', 'remote', 'shell', 'clipboard', 'screen', 'tray', 'menu', 'auto-updater', 'ipc-renderer', 'ipc-main'];
 
 module.exports = function redirect(request) {
   // Tell valiquire to ignore these modules
