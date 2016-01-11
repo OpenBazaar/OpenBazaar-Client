7baf9af02dd30fa4ffc0579eceea98ac65b1dc7a
Brian Hoffman <brianchoffman@gmail.com>
2016-01-11 15:24:54 -0500

    Fix up menubar
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Menubar and App Menu Updates
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Menubar and App Menu Updates
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    Add menu,tray,auto-updater to Electron deps
    
    Re-enable Copy Paste
    
    Re-enable Copy Paste
    
    Handle URIs for stores and items
    
    add ipc modules
    
    Fix up menubar

--- main.js
+++ main.js
@@ -8,32 +8,40 @@ var fs = require('fs');
 var path = require('path');
 
 var app = require('app');  // Module to control application life.
-var BrowserWindow = require('electron').BrowserWindow;  // Module to create native browser window.
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 var request = require('request');
 var os = require('os');
 var autoUpdater = require('auto-updater');
-var electron = require('electron');
 var menu = require('menu');
 var tray = require('tray');
+var ipcMain = require('ipc-main');
 
 var launched_from_installer = false;
-var platform = os.platform() + '_' + os.arch();
+var platform = os.platform();
 switch(platform) {
-  case "darwin_x64":
+  case "darwin":
     platform = "mac";
 }
 var version = app.getVersion();
 var trayMenu = null;
 var subpy = null;
 
-var start_local_server = function() {
-  var platform = process.platform;
+var open_url = null; // This is for if someone opens a URL before the client is open
 
-  if(platform == "darwin" || platform == "linux") {
-    subpy = require('child_process').spawn('./openbazaard', ['start', '--testnet', '--loglevel', 'debug'], {
+// Set daemon binary name
+var daemon = "openbazaard.exe";
+if(platform == "mac" || platform == "linux") {
+  daemon = "openbazaard";
+}
+
+var start_local_server = function() {
+  if(fs.existsSync(__dirname + path.sep + '..' + path.sep + 'OpenBazaar-Server' + path.sep + daemon)) {
+    subpy = require('child_process').spawn(daemon, ['start', '--testnet', '--loglevel', 'debug'], {
       detach: true,
-      cwd: __dirname + '/OpenBazaar-Server'
+      cwd: __dirname + path.sep + '..' + path.sep + 'OpenBazaar-Server'
     });
+
     var stdout = '';
     var stderr = '';
 
@@ -45,7 +53,7 @@ var start_local_server = function() {
       console.log('[STR] stderr "%s"', String(buf));
       stderr += buf;
     });
-    subpy.on('error', function(err) {
+    subpy.on('error', function (err) {
       console.log('Python error %s', String(err));
     });
     subpy.on('close', function (code) {
@@ -58,9 +66,8 @@ var start_local_server = function() {
 };
 
 // Check if we need to kick off the python server-daemon (Desktop app)
-if(fs.existsSync(__dirname + path.sep + "OpenBazaar-Server")) {
+if(fs.existsSync(__dirname + path.sep + ".." + path.sep + "OpenBazaar-Server" + daemon)) {
   launched_from_installer = true;
-
   console.log('Starting OpenBazaar Server');
   start_local_server();
 }
@@ -76,7 +83,7 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  if (platform != 'mac') {
     app.quit();
   }
 });
@@ -101,15 +108,6 @@ app.on('before-quit', function (e) {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
 
-  //var protocol = require('protocol');
-  //protocol.registerProtocol('ob', function(request) {
-  //  var url = request.url.substr(5);
-  //  console.log(path.normalize(__dirname + '/' + url));
-  //}, function (error) {
-  //  if (error)
-  //    console.error('Failed to register protocol')
-  //});
-
   // Application Menu
   var appMenu = menu.buildFromTemplate([
     {
@@ -122,6 +120,18 @@ app.on('ready', function() {
             app.quit();
           }
         }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
       ]
     },
     {
@@ -163,7 +173,7 @@ app.on('ready', function() {
         {
           label: 'Toggle Developer Tools',
           accelerator: (function() {
-            if (process.platform == 'darwin') {
+            if (platform == 'mac') {
               return 'Alt+Command+I';
             } else {
               return 'Ctrl+Shift+I';
@@ -176,42 +186,64 @@ app.on('ready', function() {
           }
         },
       ]
-    }
+    },
+    {
+    label: 'Window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'Command+M',
+        selector: 'performMiniaturize:'
+      },
+      {
+        label: 'Close',
+        accelerator: 'Command+W',
+        selector: 'performClose:'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        selector: 'arrangeInFront:'
+      }
+    ]
+  }
   ]);
   menu.setApplicationMenu(appMenu);
 
-  // Context Menu for OS X
-  if (process.platform == 'darwin') {
-    trayMenu = new tray(__dirname + '/imgs/menubar_icon.png');
-    var contextMenu = menu.buildFromTemplate([
-      {
-        label: 'Start Local Server', type: 'normal', click: function () {
-        start_local_server();
-      }
-      },
-      {
-        label: 'Shutdown Local Server', type: 'normal', click: function () {
-        request('http://localhost:18469/api/v1/shutdown', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log('Shutting down server');
-          } else {
-            console.log('Server does not seem to be running.');
-          }
-        });
-      }
-      },
-      {type: 'separator'},
-      {label: 'View Debug Log', type: 'normal'},
-      {type: 'separator'},
-      {
-        label: 'Quit', type: 'normal', accelerator: 'Command+Q', click: function () {
-        app.quit();
-      }
-      }
-    ]);
+  trayMenu = new tray(__dirname + '/imgs/menubar_icon.png');
+  var contextMenu = menu.buildFromTemplate([
+    {
+      label: 'Start Local Server', type: 'normal', click: function () {
+      start_local_server();
+    }
+    },
+    {
+      label: 'Shutdown Local Server', type: 'normal', click: function () {
+      request('http://localhost:18469/api/v1/shutdown', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log('Shutting down server');
+        } else {
+          console.log('Server does not seem to be running.');
+        }
+      });
+    }
+    },
+    {type: 'separator'},
+    {label: 'View Debug Log', type: 'normal', click: function() {
+      // Open Debug Log Wherever It Is
 
-    trayMenu.setContextMenu(contextMenu);
-  }
+    }},
+    {type: 'separator'},
+    {
+      label: 'Quit', type: 'normal', accelerator: 'Command+Q', click: function () {
+      app.quit();
+    }
+    }
+  ]);
+
+  trayMenu.setContextMenu(contextMenu);
 
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -227,7 +259,11 @@ app.on('ready', function() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  if(open_url) {
+    mainWindow.loadURL('file://' + __dirname + '/index.html' + open_url);
+  } else {
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+  }
 
   // Open the devtools.
   mainWindow.openDevTools({detach: true});
@@ -253,18 +289,57 @@ app.on('ready', function() {
   });
 
   autoUpdater.on("update-not-available", function(msg) {
-    console.log(msg); //print msg , you can find the cash reason.
+    // Nothing to do here
   });
 
   autoUpdater.on("update-available", function() {
-    mainWindow.webContents.send('ping', 'Update available!');
+    mainWindow.webContents.executeJavaScript("console.log('Update available! Downloading now...')");
   });
 
   autoUpdater.on("update-downloaded", function(e, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
-    mainWindow.webContents.send('ping', 'Update available!');
+    mainWindow.webContents.executeJavaScript("console.log('Update downloaded." + updateUrl + "')");
+    if(platform == "mac") {
+      mainWindow.webContents.executeJavaScript('$(".js-statusBarMessage").html("Would you like to update OpenBazaar? <span class=\\"clickable js-navInstallUpdate\\">Restart</span> to update.");');
+      mainWindow.webContents.executeJavaScript('$(".js-statusBarMessage").parent().removeClass("fadeOut");');
+    }
   });
 
-  autoUpdater.setFeedUrl('http://updates.openbazaar.org:5000/update/' + platform + '/' + version);
+  ipcMain.on('installUpdate', function(event) {
+    console.log('Installing Update');
+    autoUpdater.quitAndInstall();
+  });
+
+  autoUpdater.setFeedURL('http://updates.openbazaar.org:5000/update/' + platform + '/' + version);
   autoUpdater.checkForUpdates();
 
+});
+
+app.on('open-url', function(event, uri) {
+
+  // uri should be in format ob:route delimited by colons
+  // eg: ob:user:GUID
+  //     ob:user:GUID:store
+  //     ob:user:GUID:item:ITEM_ID
+  var split_uri = uri.split(':');
+  if(split_uri.length > 1 && split_uri[0] == "ob") {
+    switch(split_uri[1]) {
+      case "user":
+        open_url = "#userPage/" + split_uri[2];
+        if(split_uri[3] == "store") {
+          open_url += "/store";
+        } else if(split_uri[3] == "item") {
+          open_url += "/item" + split_uri[4];
+        }
+
+        break;
+    }
+  }
+  console.log(open_url);
+
+  // If application was not open store in localStorage
+  if(mainWindow) {
+    mainWindow.webContents.executeJavaScript("Backbone.history.navigate('" + open_url + "', {trigger: true});");
+  }
+
+  event.preventDefault();
 });
