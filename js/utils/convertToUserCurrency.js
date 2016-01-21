4bb7e7fd1cc86e1bae92c62abdec2e9c4ea81a80
Josh Jeffryes <jjeffryes@gmail.com>
2016-01-20 22:40:41 -0500

    fix price calculations in transactions

--- js/utils/convertToUserCurrency.js
+++ js/utils/convertToUserCurrency.js
@@ -2,18 +2,19 @@ var __ = require('underscore'),
     Backbone = require('backbone');
 
 /*eslint no-use-before-define:0*/
-module.exports = function (priceIn, btAve, currencyCodeIn, currencyCodeOut, callback) {
+module.exports = function (priceIn, oneVendorCurrencyUnitInBTC, currencyCodeIn, currencyCodeOut, callback) {
     var self = this,
         priceInBTC = 0,
         inToOutBTCRatio = 0,
+        priceToFormat = 0,
         priceOut = 0;
 
     currencyCodeIn = currencyCodeIn.toUpperCase();
     currencyCodeOut = currencyCodeOut.toUpperCase();
 
-    priceInBTC = Number(priceIn * btAve);
-    //if currencyCodeIn and currencyCodeOut are the same, multiply by one to avoid rounding errors
-    inToOutBTCRatio = (currencyCodeIn == currencyCodeOut) ? 1 : window.currentBitcoin/btAve;
+    priceInBTC = (currencyCodeIn == "BTC") ? priceIn : Number(priceIn / oneVendorCurrencyUnitInBTC);
+    inToOutBTCRatio = (currencyCodeIn == currencyCodeOut) ? 1 : window.currentBitcoin/oneVendorCurrencyUnitInBTC;
+    priceToFormat = (currencyCodeIn == currencyCodeOut) ? priceIn : priceInBTC*inToOutBTCRatio;
 
     if(currencyCodeOut != "BTC"){
         priceOut = new Intl.NumberFormat(window.lang, {
@@ -21,7 +22,7 @@ module.exports = function (priceIn, btAve, currencyCodeIn, currencyCodeOut, call
             minimumFractionDigits: 2,
             maximumFractionDigits: 2,
             currency: currencyCodeOut
-        }).format(priceInBTC*inToOutBTCRatio);
+        }).format(priceToFormat);
     }
     typeof callback === 'function' && callback(priceOut, priceInBTC);
 };
