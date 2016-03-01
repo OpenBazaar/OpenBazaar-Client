19508badbf40527c10708d1811e0c01e8fde7b0e
Josh Jeffryes <jjeffryes@gmail.com>
2016-03-01 09:45:06 -0500

    fix links in about/support
    Closes #868

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ if(platform === "linux") {
 }
 
 //open external links in a browser, not the app
-$('body').on('click', '.js-externalLink, .about a, .js-listingDescription a', function(e){
+$('body').on('click', '.js-externalLink, .js-aboutModalHolder a, .js-listingDescription a', function(e){
   e.preventDefault();
   var extUrl = $(this).attr('href');
   if (!/^https?:\/\//i.test(extUrl)) {
