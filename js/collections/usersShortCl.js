4ee4b6b5892f449322bce9d0f4748e9c0e8b2e17
Josh Jeffryes <jjeffryes@gmail.com>
2016-05-06 17:58:37 -0400

    Remove unneeded code

--- js/collections/usersShortCl.js
+++ js/collections/usersShortCl.js
@@ -1,22 +1,6 @@
 var Backbone = require('backbone'),
-    app = require('../App.js').getApp(),
     userShort = require('../models/userShortMd');
 
 module.exports = Backbone.Collection.extend({
-  model: userShort,
-
-  initialize: function(models, options){
-
-    /*
-    if (!options.fetchURL) {
-      throw new Error('Please provide a collection.');
-    }
-    
-    this.fetchURL = options.fetchURL;*/
-  },
-
-
-  url: function() {
-    return app.serverConfigs.getActive().getServerBaseUrl() + '/' + this.fetchURL;
-  }
+  model: userShort
 });
